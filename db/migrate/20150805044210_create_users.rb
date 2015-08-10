394192faf6be29220641cb7cf7d129cb42de5a02
momoyoechigo <echigo@kamiogroup.me>
2015-08-10 12:35:18 +0000

    profileandarea

--- db/migrate/20150805044210_create_users.rb
+++ db/migrate/20150805044210_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :profile
+      t.string :area
       t.string :password_digest
 
       t.timestamps
